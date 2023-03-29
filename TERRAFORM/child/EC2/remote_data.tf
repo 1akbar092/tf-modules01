65a756397aa1562f9497d93aa70d8af0b4c209fb
1akbar092 <abdugafforovakbar55@gmail.com>
2023-03-29 16:45:01 -0400

    TERRAFORM/child/EC2/remote_data.tf

---
+++ TERRAFORM/child/EC2/remote_data.tf
@@ -0,0 +1,21 @@
+locals {
+  ami_id = data.aws_ami.amznlx2.id
+}
+
+###--------------------------------
+data "aws_ami" "amznlx2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*-gp2"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
