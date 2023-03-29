e5c8273be82c4443fff4d5cacafe47d3e7b26ac1
1akbar092 <abdugafforovakbar55@gmail.com>
2023-03-29 16:49:38 -0400

    modified:   TERRAFORM/root/main.tf

--- TERRAFORM/root/main.tf
+++ TERRAFORM/root/main.tf
@@ -1,5 +1,5 @@
 module "EC2" {
-  source        = "../child/EC2"
+  source        = "git::https://github.com/1akbar092/tf-modules01//TERRAFORM/child/EC2"
   subnet_id     = module.VPC.public_subnet_id
   sg_ids        = [module.VPC.sg_id]
   instance_type = "t2.micro"
@@ -7,6 +7,6 @@ module "EC2" {
 }
 
 module "VPC" {
-  source   = "../child/VPC"
+  source   = "git::https://github.com/1akbar092/tf-modules01//TERRAFORM/child/VPC"
   sg_ports = [80, 443, 53, 23, 3389, 22, 60, 70, 3306, 8080]
 }
