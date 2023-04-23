7e4c082d75ae8bafcd1bdd7f158bcd808281cdd8
1akbar092 <abdugafforovakbar55@gmail.com>
2023-04-22 20:57:49 -0400

    modified:   TERRAFORM/child/EC2/main.tf

--- TERRAFORM/child/EC2/main.tf
+++ TERRAFORM/child/EC2/main.tf
@@ -1,11 +1,11 @@
 ###### EC2 instance
-resource "aws_instance" "MyInstance01" {
+resource "aws_instance" "MyPublicInstance" {
   ami                         = local.ami_id
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   associate_public_ip_address = var.public_ip_ass
   security_groups             = var.sg_ids
   tags = {
-    "Name" = "MyInstance01"
+    "Name" = "MyPublicInstance"
   }
 }
