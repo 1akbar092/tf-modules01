06ea7cd4180267090e3c4fd3fe9574291bc17baa
1akbar092 <abdugafforovakbar55@gmail.com>
2023-04-22 20:56:45 -0400

    modified:   ../child/EC2/main.tf         modified:   main.tf

--- TERRAFORM/root/main.tf
+++ TERRAFORM/root/main.tf
@@ -1,5 +1,6 @@
+##### EC2 Launch in the Public Subnet
 module "EC2" {
-  source        = "git::https://github.com/1akbar092/tf-modules01//TERRAFORM/child/EC2"
+  source        = "../child/EC2"
   subnet_id     = module.VPC.public_subnet_id
   sg_ids        = [module.VPC.sg_id]
   instance_type = "t2.micro"
@@ -7,6 +8,6 @@ module "EC2" {
 }
 
 module "VPC" {
-  source   = "git::https://github.com/1akbar092/tf-modules01//TERRAFORM/child/VPC"
+  source   = "../child/VPC"
   sg_ports = [80, 443, 53, 23, 3389, 22, 60, 70, 3306, 8080]
 }
